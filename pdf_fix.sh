52f8f0fb9cea4a0439ba47a612d82a8b08847979
DmDS <1803032+DmDS@users.noreply.github.com>
2025-05-21 22:56:27 +0300

    Create pdf_fix.sh

---
+++ pdf_fix.sh
@@ -0,0 +1,2 @@
+sed -i 's/capabilities desc="Should we require capabilities to isolate processes into chroot jails" type="bool" default="true">true</capabilities desc="Should we require capabilities to isolate processes into chroot jails" type="bool" default="true">false</' /etc/coolwsd/coolwsd.xml
+serivce coolwsd restart
