a3bb8e7183c5272bb26971bb7906dab20a3577ef
DmDS <1803032+DmDS@users.noreply.github.com>
2026-06-27 10:33:30 +0300

    Delete pdf_fix.sh

--- pdf_fix.sh
+++
@@ -1,2 +0,0 @@
-sed -i 's/capabilities desc="Should we require capabilities to isolate processes into chroot jails" type="bool" default="true">true</capabilities desc="Should we require capabilities to isolate processes into chroot jails" type="bool" default="true">false</' /etc/coolwsd/coolwsd.xml
-service coolwsd restart
