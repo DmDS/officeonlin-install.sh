813e0fa1f1900042725e186f4e733bbd290f384a
DmDS <1803032+DmDS@users.noreply.github.com>
2026-06-28 10:15:43 +0300

    Update onlinePrep.sh

--- bin/onlinePrep.sh
+++ bin/onlinePrep.sh
@@ -1,47 +1,108 @@
 #!/bin/bash
 # shellcheck disable=SC2154,SC2034
 # this script contains:
-## idempotent functions to define if LibreOffice Online has to be compiled
-## Installation of requirements for Libreoffice Online build only
-## Download & install LibreOffice Online Sources
-cp /usr/local/lib/libPocoCrypto.so.* /usr/lib/
-cp /usr/local/lib/libPocoXML.so.* /usr/lib/
-set -e
-SearchGitOpts=''
-[ -n "${cool_src_branch}" ] && SearchGitOpts="${SearchGitOpts} -b ${cool_src_branch}"
-[ -n "${cool_src_commit}" ] && SearchGitOpts="${SearchGitOpts} -c ${cool_src_commit}"
-[ -n "${cool_src_tag}" ] && SearchGitOpts="${SearchGitOpts} -b ${cool_src_tag}"
-#### Download dependencies ####
-if [ -d ${cool_dir} ]; then
-  cd ${cool_dir}
-else
-  git clone ${SearchGitOpts} --single-branch ${cool_src_repo} ${cool_dir}
+## Download FULL Monorepo (Online + Engine) & Prepare Structure
+
+# Копируем собранные библиотеки Poco
+if ls /usr/local/lib/libPocoCrypto.so.* 1> /dev/null 2>&1; then
+  cp /usr/local/lib/libPocoCrypto.so.* /usr/lib/
+  cp /usr/local/lib/libPocoXML.so.* /usr/lib/
 fi
-#declare repChanged
-#eval "$(SearchGitCommit $SearchGitOpts)"
-#if [ -f ${cool_dir}/coolwsd ] && $repChanged ; then
-#  cool_forcebuild=true
-#fi
+
+set -e
+
+# ПЕРЕХВАТ СТАРОГО РЕПОЗИТОРИЯ
+if [[ -z "$cool_src_repo" || "$cool_src_repo" == *"CollaboraOnline/online.git"* ]]; then
+  cool_src_repo="https://github.com/CollaboraOnline/online.mirror.git"
+fi
+
+###############################################################################
+#### 1. ПОЛНАЯ ЗАГРУЗКА МОНОРЕПОЗИТОРИЯ (С ПОДДЕРЖКОЙ ДОКАЧКИ) ##########
+###############################################################################
+if [ -d "${cool_dir}/wsd" ] && [ -d "${cool_dir}/engine/include" ]; then
+  echo "Full monorepo already exists. Skipping download."
+else
+  echo "Preparing to download FULL monorepo (~1.5-2GB)..."
+  echo "NOTE: If download interrupts, just re-run the script. It WILL RESUME!"
+
+  if [ ! -d "${cool_dir}/.git" ]; then
+    rm -rf ${cool_dir}
+    mkdir -p ${cool_dir}
+    cd ${cool_dir}
+    git init
+    git remote add origin "${cool_src_repo}"
+    # Отключаем предупреждения о detached HEAD
+    git config advice.detachedHead false
+  else
+    cd ${cool_dir}
+  fi
+
+  # Берем ветку или тег из конфига (если ничего нет, по умолчанию main)
+  TARGET_REF="${cool_src_tag:-${cool_src_branch:-main}}"
+
+  echo "Fetching '${TARGET_REF}' (this will take a while, but is resumable)..."
+  if ! git fetch --progress --depth=1 origin "${TARGET_REF}"; then
+     echo ""
+     echo "!!! DOWNLOAD INTERRUPTED !!!"
+     echo "Don't panic. Just run ./officeonline-install2.sh again."
+     echo "Git will resume exactly from where it stopped."
+     return 1
+  fi
+
+  echo "Extracting files..."
+  git checkout FETCH_HEAD
+  chown -R cool:cool ${cool_dir}
+  echo "Full monorepo downloaded successfully!"
+fi
+
+###############################################################################
+#### 2. УСТАНОВКА ЗАВИСИМОСТЕЙ ##############################################
+###############################################################################
 if [ "${DIST}" = "Debian" ]; then
   if [ "${CODENAME}" = "bullseye" ] || [ "${CODENAME}" = "bookworm" ]; then
-    apt-get install  libssl-dev libpococrypto80 -y
+    apt-get install libssl-dev -y
   elif [ "${CODENAME}" = "buster" ]; then
-    apt-get install  libssl-dev libpococrypto60 -y
+    apt-get install libssl-dev -y
   else
-    apt-get install nodejs-dev node-gyp libssl1.0-dev npm libpococrypto80 -y
+    apt-get install nodejs-dev node-gyp libssl1.0-dev npm -y
   fi
-  else
-#  apt-get install nodejs node-gyp libssl-dev npm libpococrypto80 -y
-  apt-get install nodejs libssl-dev libpococrypto80 -y
+else
+  apt-get install nodejs libssl-dev -y
 fi
 
 set +e
 if ! npm -g list jake >/dev/null; then
-#  npm install -g npm
   npm install -g jake
 fi
 
-sed  '16a\
-#include <list>
-' < ${cool_dir}/wsd/AdminModel.hpp > ${cool_dir}/wsd/AdminModeltmp.hpp
-cat ${cool_dir}/wsd/AdminModeltmp.hpp > ${cool_dir}/wsd/AdminModel.hpp
+# Патч для старой версии (в новых main он может не понадобиться, но оставим для совместимости)
+if [ -f "${cool_dir}/wsd/AdminModel.hpp" ]; then
+  if ! grep -q "^#include <list>" "${cool_dir}/wsd/AdminModel.hpp"; then
+    sed -i '16a\#include <list>' "${cool_dir}/wsd/AdminModel.hpp"
+  fi
+fi
+
+###############################################################################
+#### 3. ПОДМЕНА ИСХОДНИКОВ ДВИЖКА НА ГОТОВЫЕ БИНАРНИКИ ####################
+###############################################################################
+set -e
+ENGINE_DIR="${cool_dir}/engine"
+ENGINE_INSTDIR="${ENGINE_DIR}/instdir"
+
+# В полном монорепозитории заголовки (COKit) и исходники уже на месте в engine/.
+# Нам нужно только положить СКОМПИЛИРОВАННЫЕ бинарники (instdir) рядом с ними.
+if [ ! -d "${ENGINE_INSTDIR}/program" ]; then
+  if [ -d "${lo_dir}/instdir" ]; then
+    echo "Moving pre-compiled engine binaries into the monorepo engine/ folder..."
+    mv "${lo_dir}/instdir" "${ENGINE_DIR}/"
+    chown -R cool:cool "${ENGINE_INSTDIR}"
+    echo "Engine binaries successfully integrated."
+  else
+    echo "ERROR: Engine binaries not found at ${lo_dir}/instdir."
+    return 1
+  fi
+else
+  echo "Engine binaries already in place. Skipping."
+fi
+
+set +e
