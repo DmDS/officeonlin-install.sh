3d31bac5be910506ca75ebd1de0f5fdc8ae17e59
DmDS <1803032+DmDS@users.noreply.github.com>
2026-06-28 21:02:45 +0300

    Update officeonline-install2.sh

--- officeonline-install2.sh
+++ officeonline-install2.sh
@@ -160,18 +160,5 @@ source "$ScriptFullPath/bin/corePrep2.sh"
   # shellcheck source=/project/bin/onlineInstall.sh
   source "$ScriptFullPath/bin/onlineInstall.sh"
 } > >(tee -a ${log_dir}/cool-compilation.log) 2> >(tee -a ${log_dir}/cool-compilation.log >&2)
-### Testing coolwsd ###
-if ${sh_interactive}; then
-  admin_pwd=$(awk -F'password=' '{printf $2}' /lib/systemd/system/${coolwsd_service_name}.service )
-  dialog --backtitle "Information" \
-  --title "Note" \
-  --msgbox "The installation logs are in ${log_dir}. After reboot you can use $coolwsd_service_name.service using: systemctl (start,stop or status) $coolwsd_service_name.service.\\n
-Your user is admin and password is $admin_pwd. Please change your user and/or password in (/lib/systemd/system/$coolwsd_service_name.service),\\n
-after that run (systemctl daemon-reload && systemctl restart $coolwsd_service_name.service).\\nPlease press OK and wait 15 sec. I will start the service." 10 145
-  clear
-fi
-{
-  # shellcheck source=/project/bin/onlineTests.sh
-#  source "$ScriptFullPath/bin/onlineTests.sh"
-#} > >(tee -a ${log_dir}/cool-compilation.log) 2> >(tee -a ${log_dir}/cool-compilation.log >&2)
+
 exit 0
