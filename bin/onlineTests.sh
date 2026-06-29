23ad0deb792cb2331d5b101860656a318dfbd183
DmDS <1803032+DmDS@users.noreply.github.com>
2026-06-29 06:54:16 +0300

    Update onlineTests.sh

--- bin/onlineTests.sh
+++ bin/onlineTests.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 # shellcheck disable=SC2154
 # tests script
-chown -R cool:cool /opt/cool/jails
+
+JAILS_DIR="/opt/cool/jails"
+
+if [ ! -d "${JAILS_DIR}" ]; then
+  echo "Jails directory not found. Creating ${JAILS_DIR}..."
+  mkdir -p "${JAILS_DIR}"
+fi
+
+CURRENT_OWNER=$(stat -c '%U:%G' "${JAILS_DIR}")
+if [ "$CURRENT_OWNER" != "cool:cool" ]; then
+  echo "Fixing permissions for ${JAILS_DIR} (current: $CURRENT_OWNER)..."
+  chown -R cool:cool "${JAILS_DIR}"
+fi
+
 systemctl start $coolwsd_service_name.service
 rm -rf ${lo_dir}/workdir
 sleep 18
+
 if pgrep -u cool coolwsd; then
   clear
   echo -e "\033[33;7m### coolwsd is running. Enjoy!!! ###\033[0m"
